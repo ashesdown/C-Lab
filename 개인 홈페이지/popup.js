73c9c556feb00ba3531d4f9ec9d1ad074f139ccd
ashesdown <yishen3251525@naver.com>
2021-12-28 09:18:53 +0900

    12/28

--- 개인 홈페이지/popup.js
+++ 개인 홈페이지/popup.js
@@ -1,30 +1,52 @@
-const openButton = document.querySelectorAll("#box1, #box2, #box3, #box4")
+const openButtons = document.querySelectorAll("#box1, #box2, #box3, #box4")
 const modal = document.querySelectorAll(".modal");
-const overlay = document.querySelectorAll(".overlay")
-const closeBtn = document.querySelectorAll("#Xbutton")
+const overlay = document.querySelectorAll(".overlay");
+const closeBtn = document.querySelectorAll("#Xbutton");
 const element = document.getElementsByClassName('content')[0];
 
 const openModal = () => {
-  modal.classList.remove("hidden");
+  console.log(modal)
+  modal[0].classList.remove("hidden");
 }
 const closeModal = () => {
-  modal.classList.add("hidden")
+  modal[0].classList.add("hidden");
 }
 function modalContent1() {
   element.innerHTML
-    = '<div style="color:blue">InnerHTML<div>';
+    = '<div><div id="img1"></div></div>';
+  modal.classList.add("modaldesign1")
 }
 
 function modalContent2() {
   element.innerHTML
-    = '<div style="color:blue">wow<div>';
+    = '<div><div>';
+    modal.classList.add("modaldesign2")
 }
 
 function modalContent3() {
   element.innerHTML
-    = '<div style="color:blue">hey<div>';
+    = '<div><div>';
+    modal.classList.add("modaldesign3")
 }
 
-overlay.addEventListener("click", closeModal);
-closeBtn.addEventListener("click", closeModal);
-openButton.addEventListener("click", openModal);
+function modalContent4() {
+  element.innerHTML
+    = '<div><div>';
+    modal.classList.add("modaldesign4")
+}
+// overlay.addEventListener("click", closeModal);
+// closeBtn.addEventListener("click", closeModal);
+
+for (var i in openButtons) {
+  var openButton = openButtons[i]
+  if (openButton.addEventListener !== undefined) {
+    openButton.addEventListener("click", openModal);
+  }
+}
+
+for (var i in closeBtn) {
+  var closeButton = closeBtn[i]
+  if (closeButton.addEventListener !== undefined) {
+    closeButton.addEventListener("click", closeModal);
+  }
+}
